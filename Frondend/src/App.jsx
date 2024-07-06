0406ce654845c8b5c8ab4e435edcb6937266f7fa
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-07-06 21:24:37 +0530

    intial commit

---
+++ Frondend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Header, Main, Message } from "./Components";
+
+function App() {
+  return <>
+    <Header/>
+    <Main/>
+    <Message/>
+  </>;
+}
+
+export default App;
